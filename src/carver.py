a0918de3b119adf0d5b6b9cc24b9c434d21b4579
Ben Tanen <ben.tanen@gmail.com>
2020-09-08 21:56:31 -0600

    (1) added jupyter notebook with code, (2) added images showing results and comparison

--- src/carver.py
+++ src/carver.py
@@ -40,6 +40,50 @@ def calc_energy(img):
 
     return energy_map
 
+@jit
+def minimum_seam(img):
+    r, c, _ = img.shape
+    energy_map = calc_energy(img)
+
+    M = energy_map.copy()
+    backtrack = np.zeros_like(M, dtype=np.int)
+
+    for i in range(1, r):
+        for j in range(0, c):
+            # Handle the left edge of the image, to ensure we don't index a -1
+            if j == 0:
+                idx = np.argmin(M[i - 1, j:j + 2])
+                backtrack[i, j] = idx + j
+                min_energy = M[i-1, idx + j]
+            else:
+                idx = np.argmin(M[i - 1, j - 1:j + 2])
+                backtrack[i, j] = idx + j - 1
+                min_energy = M[i - 1, idx + j - 1]
+
+            M[i, j] += min_energy
+
+    seam = np.zeros(r, dtype = np.int32)
+    j = np.argmin(M[-1])
+    for i in reversed(range(r)):
+        seam[i] = j
+        j = backtrack[i, j]
+
+    return M, backtrack, seam
+
+@jit
+def carve_column(img):
+    r, c, _ = img.shape
+
+    M, backtrack, seam = minimum_seam(img)
+    mask = np.ones((r, c), dtype = np.bool)
+
+    for i in range(r):
+        mask[i, seam[i]] = False
+
+    mask = np.stack([mask] * 3, axis=2)
+    img = img[mask].reshape((r, c - 1, 3))
+    return img
+
 def crop_c(img, scale_c):
     r, c, _ = img.shape
     new_c = int(scale_c * c)
@@ -54,46 +98,6 @@ def crop_r(img, scale_r):
     img = crop_c(img, scale_r)
     img = np.rot90(img, 3, (0, 1))
     return img
-
-@jit
-def carve_column(img):
-    r, c, _ = img.shape
-
-    M, backtrack = minimum_seam(img)
-    mask = np.ones((r, c), dtype=np.bool)
-
-    j = np.argmin(M[-1])
-    for i in reversed(range(r)):
-        mask[i, j] = False
-        j = backtrack[i, j]
-
-    mask = np.stack([mask] * 3, axis=2)
-    img = img[mask].reshape((r, c - 1, 3))
-    return img
-
-@jit
-def minimum_seam(img):
-    r, c, _ = img.shape
-    energy_map = calc_energy(img)
-
-    M = energy_map.copy()
-    backtrack = np.zeros_like(M, dtype=np.int)
-
-    for i in range(1, r):
-        for j in range(0, c):
-            # Handle the left edge of the image, to ensure we don't index a -1
-            if j == 0:
-                idx = np.argmin(M[i-1, j:j + 2])
-                backtrack[i, j] = idx + j
-                min_energy = M[i-1, idx + j]
-            else:
-                idx = np.argmin(M[i - 1, j - 1:j + 2])
-                backtrack[i, j] = idx + j - 1
-                min_energy = M[i - 1, idx + j - 1]
-
-            M[i, j] += min_energy
-
-    return M, backtrack
 
 def main():
     if len(sys.argv) != 5:
